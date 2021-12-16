f462b28b58ee690044f0d5c75b558a042ec24e54
Kevin Do <kevind0501@gmail.com>
2021-12-15 17:32:52 -0800

    added links to books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+
+      <BrowserRouter>
+        <header>
+          <NavLink to="/books" data-testid="book-link">
+            Books
+          </NavLink>
+        </header>
+
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route exact path="/books">
+            <BookList />
+          </Route>
+          <Route exact path="/books/:id"></Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
