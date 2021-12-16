a4b5cefb8245041865b7da8c603565df02c65b3c
Kevin Do <kevind0501@gmail.com>
2021-12-16 11:21:19 -0800

    working book components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BookList from './views/Books/BookList';
 import './App.css';
 import { Switch } from 'react-router-dom';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import BookDetail from './views/Books/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
@@ -12,16 +13,16 @@ function App() {
       <BrowserRouter>
         <header>
           <NavLink to="/books" data-testid="book-link">
-            Books
+            Home
           </NavLink>
         </header>
 
         <Switch>
-          <Route exact path="/"></Route>
-          <Route exact path="/books">
+          <Route exact path="/" />
+          <Route exact path="/books" component={BookList}>
             <BookList />
           </Route>
-          <Route exact path="/books/:id"></Route>
+          <Route exact path="/books/:id" component={BookDetail} />
         </Switch>
       </BrowserRouter>
     </main>
